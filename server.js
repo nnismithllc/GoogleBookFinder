09b8fe361673b9327c3c9f5e9959dadf7914fa71
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-11-22 20:45:27 -0500

    updated comma for deployment - working app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooksfinder"
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooksfinder",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
